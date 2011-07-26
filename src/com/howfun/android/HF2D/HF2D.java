ac92e2a9cb2e893eca4e2c47a12dea6310139b0a
herbertdai <herbert5069@yahoo.com.cn>
2011-07-26 21:46:44 +0800

    check horizon line

--- src/com/howfun/android/HF2D/HF2D.java
+++ src/com/howfun/android/HF2D/HF2D.java
@@ -4,59 +4,64 @@ import com.howfun.android.antguide.Utils;
 
 import android.graphics.Rect;
 
-
 public class HF2D {
 
    private static final String TAG = "HF2D";
+   private static final int YUZHI = 6;
 
    /*
     * Get next pos by angle and speed
-    * @param[in, out]: mPos   current position, it will be updated. 
-    * @param[in]: speed       speed per step
-    * @param[in]:angle        angle to move
+    * 
+    * @param[in, out]: mPos current position, it will be updated.
+    * 
+    * @param[in]: speed speed per step
+    * 
+    * @param[in]:angle angle to move
     */
    public static void getNextPos(Pos mPos, float speed, float angle) {
-      
-      double radians = Math.toRadians(angle); 
+
+      double radians = Math.toRadians(angle);
       float xStep = (float) (mPos.x + speed * Math.cos(radians));
       float yStep = (float) (mPos.y + speed * Math.sin(radians));
       mPos.x = xStep;
       mPos.y = yStep;
-      
+
    }
-   
+
    /*
-    * Check collision of rect and line
-    * if collision, then set a new angle
-    * @return true   collision
+    * Check collision of rect and line if collision, then set a new angle
+    * 
+    * @return true collision
     */
    public static boolean checkCollision(AntSprite ant, LineSprite line) {
-      
+
       if (ant == null || line == null) {
          Utils.log(TAG, "ant or line is null in checkCollision()");
          return false;
       }
       float oldAngle = ant.getAngle();
-      
-      //Left two dot of rect and upper right dot
+      float randomDegree = (float) (Math.random() * 180);
+
+      // Left two dot of rect and upper right dot
       float x1 = ant.mRect.left;
       float y1 = ant.mRect.top;
-      float x2 = x1; 
+      float x2 = x1;
       float y2 = ant.mRect.top + ant.mRect.height();
       float x3 = ant.mRect.left + ant.mRect.width();
       float y3 = y1;
-      
-      //Line's two dot
+
+      // Line's two dot
       float lax = line.getStart().x;
       float lay = line.getStart().y;
       float lbx = line.getEnd().x;
       float lby = line.getEnd().y;
-         
+
       boolean isCollision = false;
       boolean isUpperCollision = false;
-      //check upper horizontal line
-      float rx = (y1 - lay) * (lbx - lax) / (lby -lay) + lax;
-      
+
+      // check upper horizontal side
+      float rx = (y1 - lay) * (lbx - lax) / (lby - lay) + lax;
+
       float a = 0, b = 0;
       if (lax < lbx) {
          a = lax;
@@ -65,38 +70,35 @@ public class HF2D {
          b = lax;
          a = lbx;
       }
-      if (rx >= x1 && rx <= x3 
-            && rx >= a && rx <= b) {
-         isCollision = true; 
+      if (rx >= x1 && rx <= x3 && rx >= a && rx <= b) {
+         isCollision = true;
          isUpperCollision = true;
-         
+
       }
-      //check lower horizontal line
-      rx = (y2 - lay) * (lbx - lax) / (lby -lay) + lax;
-      if (rx >= x1 && rx <= x3 &&
-            (rx >= a && rx <= b)) {
-         isCollision = true; 
+      // check lower horizontal side
+      rx = (y2 - lay) * (lbx - lax) / (lby - lay) + lax;
+      if (rx >= x1 && rx <= x3 && (rx >= a && rx <= b)) {
+         isCollision = true;
       }
-      
+
       if (isCollision) {
-         //set line two point a new sequence: smaller up, bigger down
+         // set line two point a new sequence: smaller up, bigger down
          if (lay > lby) {
-            //swap
+            // swap
             float temp = lax;
             lax = lbx;
             lbx = temp;
-            
+
             temp = lay;
             lay = lby;
             lby = temp;
          }
-         
-         double lineAngle = Math.atan2((lby - lay),  (lbx - lax));
+
+         double lineAngle = Math.atan2((lby - lay), (lbx - lax));
          float degree = (float) Math.toDegrees(lineAngle);
-         float randomDegree = (float) (Math.random() * 180);
          float angle = degree + randomDegree;
-         
-         if (lax < lbx) { //'\' line
+
+         if (lax < lbx) { // '\' line
             if (!isUpperCollision) {
                angle += 180;
             }
@@ -105,58 +107,70 @@ public class HF2D {
                angle += 180;
             }
          }
-         
-         
-         //Check horizon and vertical line
-         int YUZHI = 6;
-         //vertical
-         if (Math.abs(lax - lbx) <=YUZHI) {
-            Utils.log(TAG ,"vertical line");
-            
+
+         // Check vertical line
+         if (Math.abs(lax - lbx) <= YUZHI) {
+            Utils.log(TAG, "vertical line");
+
             angle = 90 + randomDegree;
-            
-            //verify origin direction: if old and new on same PI, change to another PI 
+
+            // verify origin direction: if old and new on same PI, change to
+            // another PI
             if (oldAngle > 90 && oldAngle < 270) {
                Utils.log(TAG, "change PI");
-               angle = angle + 180; 
+               angle = angle + 180;
             }
          }
-         //horizontal
-         Utils.log(TAG, "y differs: " + (lay - lby));
-         
-         if (Math.abs(lay - lby) <= YUZHI) {
+
+         ant.setAngle(angle);
+
+      }
+
+      // fix bug: horizontal line can't be checked
+      if (!isCollision) {
+         float angle = 0;
+         float ry = (x2 - lax) * (lby - lay) / (lbx - lax) + lay;
+         if (lay < lby) {
+            a = lay;
+            b = lby;
+         } else {
+            a = lby;
+            b = lay;
+         }
+         if ((ry >= y1) && (ry <= y2) && (ry >= a) && (ry <= b)) {
+            isCollision = true;
+         }
+         if (isCollision) {
+
             Utils.log(TAG, "hori line");
             angle = 0 + randomDegree;
             if (oldAngle > 0 && oldAngle < 180) {
                Utils.log(TAG, "change PI");
                angle = angle + 180;
             }
+            ant.setAngle(angle);
          }
-         
-         ant.setAngle(angle );
-         
       }
-      
-      
+
       return isCollision;
    }
-   
+
    /*
     * check if ant is out of screen
     */
-   public static boolean checkOutOfScreen(AntSprite ant, int screenWidth,int screenHeight) {
-      
-      if ((ant.mPos.x < -AntSprite.ANT_WIDTH) 
-            ||(ant.mPos.x  > screenWidth )
-            ||(ant.mPos.y < -AntSprite.ANT_HEIGHT)
+   public static boolean checkOutOfScreen(AntSprite ant, int screenWidth,
+         int screenHeight) {
+
+      if ((ant.mPos.x < -AntSprite.ANT_WIDTH) || (ant.mPos.x > screenWidth)
+            || (ant.mPos.y < -AntSprite.ANT_HEIGHT)
             || (ant.mPos.y > screenHeight)) {
-         return true; 
+         return true;
       }
       return false;
    }
 
    /*
-    * Check collision of two rect 
+    * Check collision of two rect
     */
    public static boolean checkCollision(Rect r1, Rect r2) {
       return false;
@@ -164,13 +178,15 @@ public class HF2D {
 
    /*
     * Calculate rect by center point
-    * @param[in, out] mRect   rect to calc
-    * @param[in]     mPos     center point
+    * 
+    * @param[in, out] mRect rect to calc
+    * 
+    * @param[in] mPos center point
     */
    public static void calRectByPos(Rect mRect, Pos mPos, int width, int height) {
-      mRect.left = (int) (mPos.x - width / 2); 
+      mRect.left = (int) (mPos.x - width / 2);
       mRect.right = mRect.left + width;
-      mRect.top = (int) (mPos.y - height /2);
+      mRect.top = (int) (mPos.y - height / 2);
       mRect.bottom = mRect.top + height;
    }
 }
